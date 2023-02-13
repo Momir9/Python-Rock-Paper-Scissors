b1d435e1c07576fd8c098d09dd2e032e6caf473b
Momir <104904278+Momir9@users.noreply.github.com>
2023-02-13 05:47:16 +0100

    Add files via upload

---
+++ rock-paper-scissors.py
@@ -0,0 +1,40 @@
+import random #importing modules
+
+user_wins = 0 #variable
+computer_wins = 0 #variable
+
+options = ["rock", "paper", "scissors"] #we can also add this to line 13 instead of declaring
+
+
+while True:
+    user_input = input("Type Rock/Paper/Scissors or Q to quit.").lower()# ask the user for input, convert to lowercase
+    if user_input == "q":
+        break
+
+if user_input not in options: #list
+    continue #if the user does not type anything from the list, it returns for input
+
+random_number = random.randint(0, 2)
+#rock: 0, paper: 1, scissors: 2
+computer_pick = options[random_number]
+print("Computer picked", computer_pick + ".")
+
+if user_input == "rock" and computer_pick == "scissors":
+    print("You won")
+    user_wins += 1 #one win is added
+    
+elif user_input == "paper" and computer_pick == "rock":
+    print("You won")
+    user_wins += 1 #one win is added
+    
+elif user_input == "scissors" and computer_pick == "paper":
+    print("You won")
+    user_wins += 1 #one win is added
+    
+else:
+    print("You lost")
+    computer_wins += 1
+
+print("You won", user_wins, "times.")
+print("The computer won", computer_wins, "times.")
+print("Goodbye")
